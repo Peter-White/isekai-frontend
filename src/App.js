c5836c9fb7acc1a31a369ab78d25a2fa425fe014
Peter-White <pwhitedeveloper@gmail.com>
2022-02-16 21:53:24 -0600

    Root route added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Isekai</h1>
-      </header>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
+  );
+}
+
+const Home = () => {
+  return(
+    <header className="App-header">
+      <h1>Welcome to Isekai</h1>
+    </header>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
