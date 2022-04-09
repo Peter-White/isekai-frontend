1975753d2f51a4bfa7bc59579cb5e6a586256ff1
Peter-White <pwhitedeveloper@gmail.com>
2022-04-08 21:32:24 -0500

    Links added

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -9,6 +9,12 @@ const Home = () => {
     return(
       <header className="App-header">
         <h1>Welcome to Isekai</h1>
+        <ul>
+          <li><a href='https://www.meetup.com/isekai/'>Meetup</a></li>
+          <li><a href='discord.gg/sanantonioisekai'>Discord</a></li>
+          <li><a href='https://instagram.com/sa_isekai?utm_medium=copy_link'>Instagram</a></li>
+          <li><a href='https://www.facebook.com/groups/640985373710212/'>Facebook</a></li>
+        </ul>
       </header>
     );
   }
